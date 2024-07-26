0c3dd245f7d5583f91ad4bf8498ae53406bcffb0
unknown <bbdostoevskij@gmail.com>
2024-07-26 23:00:56 +0300

    add classwork 2: functions

---
+++ lesson2/classwork.js
@@ -0,0 +1,28 @@
+function introduceMyselfWithName(name) {
+    console.log("name:", name);
+    console.log("age: 18");
+    console.log("education: pedic");
+    console.log("religion: serduckism");
+    console.log("credo: glory to Serduck and that's it");
+}
+
+function introduceMyself() {
+    introduceMyselfWithName("Gogi")
+}
+
+function introduceMyselfAssFemGogi() {
+    introduceMyselfWithName("FemGogi")
+}
+
+introduceMyselfWithName("Vasiyl")
+
+console.log("######################################");
+
+introduceMyself()
+
+introduceMyselfAssFemGogi()
+
+introduceMyselfWithName("Gogi")
+
+
+
